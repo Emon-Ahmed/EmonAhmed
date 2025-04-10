401e69d02cfe74c92ab39817aa2c42b1618dd409
Emon-Ahmed <technicalboyemon@gmail.com>
2025-04-10 20:56:35 +0600

    docs: update profile detials & projects

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -8,12 +8,12 @@ export const DATA = {
   location: "Dhaka, Bangladesh",
   locationLink: "https://maps.app.goo.gl/HbGYWKmvZHWApKZ96",
   description:
-    "Frontend Software Engineer | Building Scalable, Responsive, User-Friendly Web Apps",
+    "Backend Engineer | JavaScript | React.js & Node.js Specialist",
   summary:
-    "Hello! I'm Emon, a **developer** specializing in the **MERN STACK** with a strong foundation in building scalable, responsive, and user-friendly web applications. I recently graduated with a **Computer Science & Engineering** degree from a private university. Passionate about adapting to new environments and technologies. With experience in both front-end and back-end development, I take a hands-on approach to **solving problems** and collaborating with others.",
+    "Backend Developer proficient in **Node.js**, **Express.js**, **MongoDB**, and **Mongoose**, with experience building scalable server-sideapplications and efficient **REST APIs**. Proficient in **database design**, **authentication**, and performance **optimization**",
   avatarUrl: "/Emon.jpg",
   skills: [
-    "JavaScript", "Bootstrap", "TailwindCSS", "React.js", "Next.js", "Redux.js", "Redux-Toolkit", " RTK Query", "Node.js", "Express.js", "REST APIs", "MongoDB", "Mongoose", "React Native", "Expo", "NativeWind", "Git", "Github", "VS Code", "Figma", "JIRA", "Vercel", "Netlify"
+    "JavaScript", "TypeScript", "Node.js", "Express.js", " REST APIs", "JWT Auth", "React.js", "Redux.js", "Redux-Toolkit", "Next.js", "TailwindCSS", "MongoDB", "Mongoose", "Mongoose", "Git", "Github", "Postman", "Zoho", "Trello", "Jira",
   ],
   navbar: [
     { href: "/", icon: HomeIcon, label: "Home" },
@@ -67,29 +67,29 @@ export const DATA = {
 
   work: [
     {
-      company: "Atomic Finance",
-      href: "https://atomic.finance",
-      badges: [],
+      company: "Salt Innovate ",
+      href: "https://saltinnovate.com/",
+      badges: ["Internship"],
       location: "Remote",
-      title: "Bitcoin Protocol Engineer",
-      logoUrl: "/atomic.png",
-      start: "May 2021",
-      end: "Oct 2022",
+      title: "Trainee Backend Engineer",
+      logoUrl: "/salt-innovate.png",
+      start: "December 2024",
+      end: "March 2025",
       description:
-        "Implemented the Bitcoin discreet log contract (DLC) protocol specifications as an open source Typescript SDK. Dockerized all microservices and setup production kubernetes cluster. Architected a data lake using AWS S3 and Athena for historical backtesting of bitcoin trading strategies. Built a mobile app using react native and typescript.",
+        "Contributed to building scalable backend services using Node.js, Express, and Next.js. Designed and integrated RESTful APIs to enable seamless frontend-backend communication. Collaborated on real-world projects, including a Hiring Manager platform, a young school website, and an Islamic EdTech solution.",
     },
-    {
-      company: "Shopify",
-      badges: [],
-      href: "https://shopify.com",
-      location: "Remote",
-      title: "Software Engineer",
-      logoUrl: "/shopify.svg",
-      start: "January 2021",
-      end: "April 2021",
-      description:
-        "Implemented a custom Kubernetes controller in Go to automate the deployment of MySQL and ProxySQL custom resources in order to enable 2,000+ internal developers to instantly deploy their app databases to production. Wrote several scripts in Go to automate MySQL database failovers while maintaining master-slave replication topologies and keeping Zookeeper nodes consistent with changes.",
-    },
+    // {
+    //   company: "Shopify",
+    //   badges: [],
+    //   href: "https://shopify.com",
+    //   location: "Remote",
+    //   title: "Software Engineer",
+    //   logoUrl: "/shopify.svg",
+    //   start: "January 2021",
+    //   end: "April 2021",
+    //   description:
+    //     "Implemented a custom Kubernetes controller in Go to automate the deployment of MySQL and ProxySQL custom resources in order to enable 2,000+ internal developers to instantly deploy their app databases to production. Wrote several scripts in Go to automate MySQL database failovers while maintaining master-slave replication topologies and keeping Zookeeper nodes consistent with changes.",
+    // },
   ],
   education: [
     {
@@ -110,17 +110,17 @@ export const DATA = {
     },
   ],
   certifications: [
-    {
-      school: "Learn with Sumit",
-      href: "https://learnwithsumit.com/",
-      degree: "Reactive Accelerator",
-      logoUrl: "/sumit.png",
-      start: "October 2024",
-      end: "Present",
-    },
+    // {
+    //   school: "Learn with Sumit",
+    //   href: "https://learnwithsumit.com/",
+    //   degree: "Reactive Accelerator",
+    //   logoUrl: "/sumit.png",
+    //   start: "October 2024",
+    //   end: "Present",
+    // },
     {
       school: "Programming Hero",
-      href: "https://web.programming-hero.com/",
+      href: "https://web.programming-hero.com/congrats/complete/61bdfafcf5118b71ade5a68a",
       degree: "Complete Web Development",
       logoUrl: "/phero.png",
       start: "June 2021",
@@ -128,7 +128,7 @@ export const DATA = {
     },
     {
       school: "Creative IT Institute",
-      href: "https://www.creativeitinstitute.com/",
+      href: "https://www.youtube.com/watch?v=ukvkio3Km_E",
       degree: "Responsive Web Design",
       logoUrl: "/creative-it.png",
       start: "August 2019",
@@ -137,64 +137,124 @@ export const DATA = {
   ],
   projects: [
     {
-      title: "JobHunt Web Application",
-      href: "https://jobhunt-bangladesh.vercel.app/",
-      dates: "Jan 2024 - Feb 2024",
+      title: "Earl Brown Education Consulting",
+      href: "https://www.ebedc.com/",
+      dates: "Official Work",
       active: true,
       description:
-        "Developed a full-stack job platform using the Mern stack and Next.js, with NextAuth for secure authentication, MongoDB for data management, and Vercel for deployment. Implemented user roles for applicants and recruiters, allowing job postings, company profiles, and profile management",
+        "Optimized app structure and data flow with Next.js, ensuring seamless frontend-backend integration and maintainable, scalable architecture through component-driven development.",
       technologies: [
+        "React.js",
         "Next.js",
-        "Next-Auth",
         "TailwindCSS",
-        "Shadcn UI",
       ],
       links: [
         {
           type: "Website",
-          href: "https://jobhunt-bangladesh.vercel.app/",
+          href: "https://www.ebedc.com/",
           icon: <Icons.globe className="size-3" />,
         },
-        {
-          type: "Source",
-          href: "https://github.com/Emon-Ahmed/JobHunt-Bangladesh",
-          icon: <Icons.github className="size-3" />,
-        },
+        // {
+        //   type: "Source",
+        //   href: "https://github.com/Emon-Ahmed/",
+        //   icon: <Icons.github className="size-3" />,
+        // },
       ],
-      image: "/jobhunt.png",
+      image: "/earl-brown-education-banner.png",
+      video: "#",
+    },
+
+    {
+      title: "AAL Young Kingz Website",
+      href: "https://www.aalyoungkingz.org/",
+      dates: "Official Work",
+      active: true,
+      description:
+        " Contributed to a school website's foundation using Next.js, enabling future backend integration with a component-based, scalable structure and optimized for server-side data handling.",
+      technologies: [
+        "React.js",
+        "Next.js",
+        "TailwindCSS",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://www.aalyoungkingz.org/",
+          icon: <Icons.globe className="size-3" />,
+        },
+        // {
+        //   type: "Source",
+        //   href: "https://github.com/Emon-Ahmed/",
+        //   icon: <Icons.github className="size-3" />,
+        // },
+        // {
+        //   type: "Server",
+        //   href: "https://github.com/Emon-Ahmed/",
+        //   icon: <Icons.github className="size-3" />,
+        // },
+      ],
+      image: "/aal-young-kingz-website-banner.png",
       video: "#",
     },
     {
-      title: "Talk Web Blog Website",
-      href: "https://talk-web-blog-website.vercel.app/",
-      dates: "June 2023 - July 2023",
+      title: "Minimal Tech Blog Website",
+      href: "https://minimal-tech-blog.vercel.app/",
+      dates: "Personal Project",
       active: true,
       description:
-        "Developed a Single Blog Application using the Mern stack with Firebase Authentication for user sign-in and sign-up functionality, ensuring seamless and secure user management. Implemented CRUD Operations allowing users to create, update, and delete blog posts, and a responsive design",
+        "Implemented scalable state and data fetching with Redux Toolkit and RTK Query, ensuring structured API flow and consistent blog rendering across components.",
       technologies: [
         "React.js",
-        "Firebase",
-        "NodeJS",
-        "ExpressJS",
+        "Redux.js",
+        "Redux Toolkit",
+        "TailwindCSS",
       ],
       links: [
         {
           type: "Website",
-          href: "https://talk-web-blog-website.vercel.app/",
+          href: "https://minimal-tech-blog.vercel.app/",
           icon: <Icons.globe className="size-3" />,
         },
         {
           type: "Source",
-          href: "https://github.com/Emon-Ahmed/TalkWeb-Blog-Website",
+          href: "https://github.com/Emon-Ahmed/Minimal-Tech-Blog",
           icon: <Icons.github className="size-3" />,
         },
         {
           type: "Server",
-          href: "https://github.com/Emon-Ahmed/TalkWeb-Blog-Website-Server",
+          href: "https://github.com/Emon-Ahmed/Minimal-Tech-Blog-Server",
           icon: <Icons.github className="size-3" />,
         },
       ],
-      image: "/talk.png",
+      image: "/minimal-tech-blog-banner.png",
+      video: "#",
+    },
+    {
+      title: "On Time Hiring - Hiring Solution",
+      href: "http://ontimehiring.com/",
+      dates: "Official Work",
+      active: true,
+      description:
+        "Built a backend system with Node.js, Express, and MongoDB using modular RESTful APIs, JWT auth, and MVC routing for user, job, and applicant management.",
+      technologies: [
+        "Node.js",
+        "Express.js",
+        "React.js",
+        "TailwindCSS",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "http://ontimehiring.com/",
+          icon: <Icons.globe className="size-3" />,
+        },
+        // {
+        //   type: "Source",
+        //   href: "https://github.com/Emon-Ahmed/",
+        //   icon: <Icons.github className="size-3" />,
+        // },
+      ],
+      image: "/ontimehiring-website-banner.png",
       video: "#",
     },
   ],
